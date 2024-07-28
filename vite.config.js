e6f9bf3a6ec331db9bb0779e8c23b5d1af4204dc
Ontrend <itontrend@gmail.com>
2024-07-28 15:59:18 +0400

    Setup CI/CD for Firebase Hosting

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,36 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'TechERP',
+        short_name: 'POS',
+        description: 'A Progressive Web App for POS',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: './images/img-1.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: './images/img-1.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   server: {
-    host: true, // Enable the server to be accessible externally
-    port: 3000, // Specify a port number (optional)
-  },
+    host: true,
+    port: 3000
+  }
 });
